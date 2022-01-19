cc5b9d2bc88c44f5774abcbe4a58c4e17d6dbf08
Your Name <you@example.com>
2022-01-19 13:50:04 -0700

    first commit

---
+++ src/components/TopNav.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-container>
+        <div class="d-flex justify-space-around mb-4">
+            <!-- Mobile Menu Icon -->
+             <v-btn id="toggleBtn"
+            class="mx-2 pa-9"
+            fab
+            color="teal"
+            >
+                <v-icon dark dense color="yellow" >
+                    I
+                </v-icon>
+            </v-btn>
+
+            <v-btn id="navBtn"
+            v-for="link in links"
+            :key="link"
+            text
+            >
+                {{ link }}
+            </v-btn>
+        </div>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "TopNav",
+        data: () => ({
+            links: [
+                'Skills',
+                'Projects',
+                'Resume',
+                'Contact',
+            ],
+        })
+    }
+</script>
+
+<style lang="scss" scoped>
+    // for small screen 
+    
+    #toggleBtn {
+            margin: 60px 10px 20px;
+            cursor: pointer;
+        }
+
+    // for medium screen 
+    @media screen and (min-width: 990px) {
+        #toggleBtn {
+            display: none;
+        }
+    }
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-sheet
+        height="60vh"
+        width="100vw"
+        class="blue text-center"
+        >
+          <div id="about" >
+            <h1>Hi, I am Jackie</h1>
+            <h1>Web Developer</h1>
+          </div>
+        </v-sheet>
+        
+        
+        <!-- Top Navigation Bar -->
+        <TopNav/>
+
+        <Skills/> 
+        <Projects/>
+        <Resume/>
+        <Contact/>  
+      </v-main>
+    </v-app>
+</template>
+
+<script>
+import TopNav from '../components/TopNav.vue';
+import Skills from '../components/Skills.vue';
+import Projects from '../components/Projects.vue';
+import Resume from '../components/Resume.vue';
+import Contact from '../components/Contact.vue';
+
+export default {
+  name: 'Home',
+  components: { 
+    TopNav,
+    Skills,
+    Projects,
+    Resume,
+    Contact
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
