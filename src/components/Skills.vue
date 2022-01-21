fe479f381d34d44aada266fa2040a98d022ae022
Your Name <you@example.com>
2022-01-21 16:24:17 -0700

    added icons to skills section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,7 +1,58 @@
 <template>
-    <div>
-        <!-- <h1>Skills</h1> -->
-    </div>
+    <v-container>
+        <v-row>
+            <v-col cols="4" class="ml-12 mt-12">
+                <h1>Skills</h1>
+            </v-col>
+        </v-row>
+
+        <v-row justify="space-around">
+            <v-icon
+            large
+            color="green darken-2"
+            >
+                fab fa-js-square
+            </v-icon>
+
+            <v-icon
+            large
+            color="blue darken-2"
+            >
+                fab fa-html5
+            </v-icon>
+
+            <v-icon
+            large
+            color="purple darken-2"
+            >
+                fab fa-css3-alt
+            </v-icon>
+
+            <v-icon
+            large
+            color="teal darken-2"
+            >
+                fab fa-python
+            </v-icon>
+
+            <v-icon
+            large
+            color="blue-grey darken-2"
+            >
+                fab fa-vuejs
+            </v-icon>
+
+            <v-icon
+            large
+            color="orange darken-2"
+            >
+                mdi-arrow-up-bold-box-outline
+            </v-icon>
+        </v-row>
+        
+    </v-container>
+    
+
 </template>
 
 <script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 
 Vue.use(Vuetify)
@@ -11,6 +12,8 @@ const opts = {}
 
 export default new Vuetify({
     opts,
-
+    icons: {
+        iconfont: 'fa',
+      },
 })
 
